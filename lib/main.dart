740f733dac71e2cec77f8da0c6d493731ead6e0d
Hungnguyen05 <manhhung.nguyen.nmh@gmail.com>
2022-07-04 22:41:48 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,7 @@ class MyHomePage extends StatelessWidget {
             ),
             Container(
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Column(
                     children: [
